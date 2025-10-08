641e0575633a44537d493ef210b04d3f28cf5461
SimranSahota02 <simran.sahota0202@gmail.com>
2025-10-08 15:20:57 +0100

    malamar and active fixes

--- src/features/sum-lot/sum-lot.js
+++ src/features/sum-lot/sum-lot.js
@@ -2,7 +2,7 @@ import "../../styles/tcg-guides.css";
 import ListDecks from "../../components/listDecks";
 
 function SumLot() {
-  const decks = ["BuzzRocTales", "Gardevoir GX", "Lost March"];
+  const decks = ["BuzzRocTales", "Gardevoir GX", "Lost March", "Malamar"];
   const format = "sum-lot";
 
   return (
