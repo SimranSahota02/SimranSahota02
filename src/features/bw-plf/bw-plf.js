d58d8ada02d42660e47ef9ab67ff58e622df0ff0
SimranSahota02 <simran.sahota0202@gmail.com>
2025-10-14 16:04:32 +0100

    tdk

--- src/features/bw-plf/bw-plf.js
+++ src/features/bw-plf/bw-plf.js
@@ -2,7 +2,7 @@ import "../../styles/tcg-guides.css";
 import ListDecks from "../../components/listDecks";
 
 function BwPlf() {
-  const decks = ["TDK", "RayEels"];
+  const decks = ["RayEels", "TDK"];
   const format = "bw-plf";
 
   return (
